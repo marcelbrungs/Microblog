822321b053c8fdc06037c22d00c80d6979c0ff14
Marcel Brungs <93032899+marcelbrungs@users.noreply.github.com>
2022-02-05 13:41:23 +0100

    Add files via upload

---
+++ Lab 3/Task 1/create-db.sql
@@ -0,0 +1,13 @@
+drop schema public cascade;
+drop table user if exists;
+create table user(
+….
+);
+drop table follower if exists;
+create table follower (
+….
+);
+drop table post if exists;
+create table post (
+…..
+);
