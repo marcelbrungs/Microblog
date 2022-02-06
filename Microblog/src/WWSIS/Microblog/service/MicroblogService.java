d877082e8732d6386a78b4df7186f0740c8afbc7
Marcel Brungs <93032899+marcelbrungs@users.noreply.github.com>
2022-02-06 22:22:22 +0100

    Update MicroblogService.java

--- Microblog/src/WWSIS/Microblog/service/MicroblogService.java
+++ Microblog/src/WWSIS/Microblog/service/MicroblogService.java
@@ -1,20 +1,27 @@
 package WWSIS.Microblog.service;
-
-public interface MicroblogService {
-
-	import java.util.List;
-	import WWSIS.Microblog.model.Follower;
-	import WWSIS.Microblog.model.User;
-	import WWSIS.Microblog.model.Post;
-	public interface MicroblogService {
-	/**
-	* Download all messages published by a specific user, and
-	* all messages from other users I follow
-	*
-	* @param XXX
-	* @return
-	*/
-	List<Post> getXXX(XXX XXX);
-	…
-	}
+import java.util.Date;
+import java.util.List;
+import WWSIS.Microblog.model.Follower;
+import WWSIS.Microblog.model.User;
+import WWSIS.Microblog.model.Post;
+public interface MicroblogService { 
+	boolean userExists(String name);
+	String registerUser(String username, String plainTextPassword);
+	List<String> searchForName(String username);
+	String createPost(String username, Post post);
+	public class PostDaoImpl implements PostDao {
+    	public String getUserTimeline(int uid) {
+        return null;}
+    	public String getMainTimeline(int uid) {
+        return null;}
+    	public String getAllMessages() {
+        return null;}
+    	public void addMessage(int uid) {}
+}
+	List<UserPostDto> getFollowersPostsForUser(String username, Date createdAfter);
+	Page<Post> getAllPostsForUsers(List<String> usernames, int pageNumber);
+	Page<Post> getAllFollowersPostsForUser(String username, int pageNumber);
+	String addFollower(String targetUsername, String followerUsername);
+	String removeFollower(String targetUsername, String followerUsername);
+	List<String> getFollowingList(String username);
 }
