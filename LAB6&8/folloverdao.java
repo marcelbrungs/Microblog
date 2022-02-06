9a7b9817edee4e736c63c410ca36c9c1b75dc6a8
krzysztof04czaja <93033751+krzysztof04czaja@users.noreply.github.com>
2022-02-06 18:59:02 +0100

    Add files via upload

---
+++ LAB6&8/folloverdao.java
@@ -0,0 +1,16 @@
+package WWSIS.Microblog.dao.impl;
+
+import WWSIS.Microblog.dao.FollowerDao;
+
+public class FollowerDaoImpl implements FollowerDao {
+public void AddPair(int followerID, int followeeID) {
+
+}
+
+public void RemovePair(int followerID, int followeeID) {
+
+}
+
+public void CheckPair(int followerID, int followeeID) {
+}
+}
