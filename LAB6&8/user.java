46cfb1b770eb73d79e6561f13d12821108f2b763
krzysztof04czaja <93033751+krzysztof04czaja@users.noreply.github.com>
2022-02-06 19:19:05 +0100

    Add files via upload

---
+++ LAB6&8/user.java
@@ -0,0 +1,10 @@
+package WWSIS.Microblog.dao.impl;
+import WWSIS.Microblog.dao.UserDao;
+
+public class UserDaoImpl implements UserDao {
+public Object getCurrentUser() {
+return null;
+}
+public void addUser() {
+}
+}
